3f33507cbabbaa13953c2220bdb6d8b9ca4a0101
I.Oukil <ilyesoukil777@gmail.com>
2025-07-19 22:03:19 +0100

    initial commit

---
+++ src/commands/commands_deploy.js
@@ -0,0 +1,35 @@
+const { REST, Routes } = require('discord.js');
+require('dotenv').config({ path: '../.env' });
+
+
+const rest = new REST().setToken(process.env.BOT_TOKEN);
+
+// append the new commands here No need to modify anything else
+const commands = [require("./joke.js").data.toJSON(), 
+                  require("./adduser.js").data.toJSON(), 
+                  require("./removeuser.js").data.toJSON()]; 
+
+// deployment
+(async function() {
+  try {
+    console.log('🔁 Deploying slash commands...');
+
+    // Option A: Per-guild (instant update)
+    await rest.put(
+      Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID),
+      { body: commands },
+    );
+
+    // Option B: Global (can take up to 1 hour)
+    // await rest.put(
+    //   Routes.applicationCommands(process.env.CLIENT_ID),
+    //   { body: commands },
+    // );
+
+    console.log('✅ Slash commands deployed successfully.');
+  } catch (error) {
+    console.error('❌ Failed to deploy commands:', error);
+  }
+})();
+
+
---
+++ src/commands/joke.js
@@ -0,0 +1,25 @@
+const {SlashCommandBuilder} = require('discord.js');
+
+module.exports = {
+    data: new SlashCommandBuilder().setName("joke").setDescription("Get a random joke"),
+    async execute(interaction) {
+        let response = await getRandomJoke();
+        if(!response) response = "i dont have a joke right now try later";
+        await interaction.reply(response);
+    }
+};
+
+async function getRandomJoke() {
+    try{
+        const response = await fetch("https://official-joke-api.appspot.com/random_joke");
+        if(!response.ok) {
+            throw "failed to retrieve joke";
+        }
+        const data = await response.json();
+        return `${data.setup}\n${data.punchline}`;
+    }
+    catch(err) {
+        console.error(err);
+    }
+}
+
